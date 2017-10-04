be7f4e4930cb7a34cfce40ff478f0d8b8f46adf5
Tiffany <tiffany.c.stallings@gmail.com>
2017-10-04 12:10:15 -0400

    Create additional locations

--- js/locations.js
+++ js/locations.js
@@ -48,5 +48,65 @@ var locations = [
 				lat: 33.9176306, 
 				lng: -84.5647575
 			}
+	},
+	{
+		title: 'Cool Beans Coffee',
+		favorite: false,
+		tags: ['restaurant', 'coffee'],
+		location: 
+			{
+				lat: 33.953122, 
+				lng:  -84.5508696
+			}
+	},
+	{
+		title: 'Thaicoon and Sushi Bar',
+		favorite: false,
+		tags: ['restaurant', 'coffee'],
+		location: 
+			{
+				lat: 33.9535084, 
+				lng:  -84.5509307
+			}
+	},
+	{
+		title: 'Martin\'s Restaurant',
+		favorite: false,
+		tags: ['restaurant', 'coffee'],
+		location: 
+			{
+				lat: 33.9206374, 
+				lng:  -84.580055
+			}
+	},
+	{
+		title: 'Starbucks',
+		favorite: false,
+		tags: ['restaurant', 'coffee'],
+		location: 
+			{
+				lat: 33.9526498, 
+				lng:  -84.551746
+			}
+	},
+	{
+		title: 'Marietta Museum of History',
+		favorite: false,
+		tags: ['historic'],
+		location: 
+			{
+				lat: 33.9526085, 
+				lng:  -84.55086279999999
+			}
+	},
+	{
+		title: 'Marietta National Cemetery',
+		favorite: false,
+		tags: ['historic'],
+		location: 
+			{
+				lat: 33.9506749, 
+				lng:  -84.53811999999999
+			}
 	}
 ];
